6374ce1f57c6ed4d624529f3342817ba257383ce
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-23 11:35:25 +0530

    add code and file

---
+++ firstproject/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    #return HttpResponse("hello,world,this is your home page")
+    return render(request,'website/index.html')
+def about(request):
+    return HttpResponse("hello,world,thsi is about page")
+def contact(request):
+    return HttpResponse("hello,world,this is your contavt page")
