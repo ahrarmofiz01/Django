a1973a27c1075c6f4b219ba1333b4890e209c7b9
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-27 14:26:08 +0530

    add code

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from .models import appvarity
+from .models import appvarity, AppReview, Store, AppCertificate
 
-# Register your models here.
-admin.site.register(appvarity)
+
+class AppReviewInline(admin.TabularInline):
+    model = AppReview
+    extra = 2
+
+
+class AppVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [AppReviewInline]
+
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)
+
+
+class AppCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+
+admin.site.register(appvarity, AppVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(AppCertificate, AppCertificateAdmin)
+
+
