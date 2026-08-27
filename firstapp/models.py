fde7f38a5568a7e692260e2d99ef9750cf5ef76e
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-27 13:00:03 +0530

    add code

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import user
 
 # Create your models here.
 class appvarity(models.Model):
@@ -16,3 +17,17 @@ class appvarity(models.Model):
 
     def __str__(self):
        return self.name
+    
+#one to many
+
+class AppReview(models.Model):
+    app= models.ForeignKey(appvarity,on_delete=models.CASCADE,related_name='reviews')
+    user=models.ForeignKey(user,on_delete=models.CASCADE)
+    rating=models.IntegerField()
+    comment=models.TextField()
+    date_added=models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+       return f'{self.user.username} review{self.app.name}'
+#MANY TO MANY
