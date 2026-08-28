482842a585e430eebca2f24cbd882ee03290c7c4
ahrar mofiz <ahrarmofiz3224@gmail.com>
2026-08-28 11:28:59 +0530

    add code

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,6 +4,7 @@ from.import views
 #localhost:800/app
 urlpatterns = [
     path('',views.all_app , name='all_app'),
+    path('app_store/',views.app_store_view,name='app_store')
     
    
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -5,3 +5,8 @@ from .models import appvarity
 def all_app(request):
     apps=appvarity.objects.all
     return render(request,'firstapp/all_app.html',{'apps':apps})
+#def app_details(request,app_id):
+    #app= get _object_or_404(appvarity,pk=app_id)
+    #return render(request,'app/app_details.html',{'app':app})
+def app_store_view(request):
+    return render(request,'firstapp/app_store.html')
